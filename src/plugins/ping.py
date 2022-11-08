57668ccdf9dc7f6b164898641bf21344ef01036d
lgc2333 <lgc2333@126.com>
2022-11-09 00:20:09 +0800

    fix

--- src/plugins/ping.py
+++ src/plugins/ping.py
@@ -3,10 +3,11 @@
 from nonebot import on_command
 from nonebot.permission import SUPERUSER
 from nonebot.matcher import Matcher
+from nonebot.adapters.onebot.v11 import MessageEvent
 
-h_ping = on_command("ping", aliases={"Ping", "PING"}, rule=SUPERUSER)
+h_ping = on_command("ping", aliases={"Ping", "PING"}, permission=SUPERUSER)
 
 
 @h_ping.handle()
-async def _(matcher: Matcher):
+async def _(matcher: Matcher, _: MessageEvent):
     await matcher.finish("Pong~")
